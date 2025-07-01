e765415c38869797300a75625488693bad065cec
nfoert <nofoert@gmail.com>
2025-06-30 21:03:39 -0400

    feat: Add event filter UI

--- scouting/static/main/scripts/admin/filters.js
+++ scouting/static/main/scripts/admin/filters.js
@@ -1,7 +1,63 @@
 document.addEventListener("alpine:init", () => {
 	Alpine.data("filters", () => ({
-		data: {},
+		data: {
+			data: [],
+			events: [],
+			users: [],
+			pits: []
+		},
 		type: "",
+		data_filters: {
+			event: {
+				open: false,
+				items: [],
+			},
+			user: {
+				open: false,
+				items: [],
+			},
+			username: {
+				open: false,
+				items: [],
+			},
+		},
+		event_filters: {
+			open: false,
+			year: {
+				open: false,
+				items: [],
+			},
+			name: {
+				open: false,
+				items: [],
+			},
+			event_code: {
+				open: false,
+				items: [],
+			},
+		},
+		user_filters: {
+			open: false,
+			username: {
+				open: false,
+				items: [],
+			},
+			team_number: {
+				open: false,
+				items: [],
+			},
+		},
+		pit_filters: {
+			open: false,
+			event: {
+				open: false,
+				items: [],
+			},
+			year: {
+				open: false,
+				items: [],
+			},
+		},
 
 		get_data() {
 			fetch(`${SERVER_IP}/get_admin_data`, {
@@ -10,12 +66,14 @@ document.addEventListener("alpine:init", () => {
 					"X-CSRFToken": CSRF_TOKEN,
 				},
 				body: JSON.stringify({
-					type: this.type,
+					type: "",
 				}),
 			})
 				.then((response) => response.json())
 				.then((data) => {
 					this.data = data;
+					
+					this.data.events_filtered = this.data.events;
 				});
 		},
 
