57b8f7d56610d70a3adf83b48ca41bcc635089e2
nfoert <nofoert@gmail.com>
2025-07-31 07:53:29 -0400

    feat: Redo notification system #99

---
+++ scouting/static/main/scripts/notifications/notifications.js
@@ -0,0 +1,51 @@
+window.addEventListener("alpine:init", () => {
+	Alpine.data("notifications", () => ({
+		notifications: [],
+		show: false,
+		title: "",
+		message: "",
+		type: "success",
+		icon: "",
+		isProcessing: false,
+
+		add_notification(title, message, type = "info", icon = "bell") {
+			this.notifications.push({ title, message, type, icon });
+		},
+
+		show_notification(title, message, type = "info", icon = "bell") {
+			this.title = title;
+			this.message = message;
+			this.type = type;
+			this.icon = `ph-bold ph-${icon}`;
+			this.show = true;
+		},
+
+		async process_queue() {
+			if (this.isProcessing) return;
+			this.isProcessing = true;
+
+			while (this.notifications.length > 0) {
+				const { title, message, type, icon } = this.notifications.shift();
+				this.show_notification(title, message, type, icon);
+				await new Promise((resolve) => setTimeout(resolve, 3000)); // show duration
+				this.show = false;
+				await new Promise((resolve) => setTimeout(resolve, 500)); // let transition finish
+			}
+
+			this.isProcessing = false;
+		},
+
+		init() {
+			window.addEventListener("scouting_notification", (event) => {
+				event.stopImmediatePropagation();
+				const { title, message, type, icon } = event.detail;
+
+				this.add_notification(title, message, type, icon);
+			});
+
+			setTimeout(() => {
+				this.process_queue();
+			}, 300);
+		},
+	}));
+});
