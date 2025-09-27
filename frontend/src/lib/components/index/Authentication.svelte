a79989bd65c74e8b50307aed95c2556073421783
nfoert <nofoert@gmail.com>
2025-09-27 08:45:51 -0400

    feat(frontend): Use tabs on the authentication step on the index page

--- frontend/src/lib/components/index/Authentication.svelte
+++ frontend/src/lib/components/index/Authentication.svelte
@@ -5,6 +5,7 @@
 	import * as Card from "$lib/components/ui/card/index.js";
     import * as Avatar from "$lib/components/ui/avatar/index.js";
     import * as Select from "$lib/components/ui/select/index.js";
+    import * as Tabs from "$lib/components/ui/tabs";
 
 	import { ArrowRight, ArrowLeft, Building, SignOut, SignIn } from "phosphor-svelte";
 	import { signOut, validateTokenOnline } from "$lib/utls/user";
@@ -36,86 +37,100 @@
 
 <div class="flex flex-col gap-4">
     <div class="flex flex-col md:flex-row gap-4">
-        <Card.Root class="max-w-128 p-4 min-w-64">
-            <Card.Header>
-                <Card.Title>Continue Without Account</Card.Title>
-                <Card.Description>Use Open Scouting without an account</Card.Description>
-            </Card.Header>
+        <Tabs.Root value="noaccount">
+            <Tabs.List>
+                <Tabs.Trigger value="noaccount">Continue Without Account</Tabs.Trigger>
+                <Tabs.Trigger value="auth">Authentication</Tabs.Trigger>
+            </Tabs.List>
 
-            <Card.Content class="flex flex-col gap-2">
-                <Label for="username">Username</Label>
-                <Input id="username" type="text" placeholder="Username"/>
+            <Tabs.Content value="noaccount">
+                <Card.Root class="max-w-128 p-4 min-w-64">
+                    <Card.Header>
+                        <Card.Title>Continue Without Account</Card.Title>
+                        <Card.Description>Use Open Scouting without an account</Card.Description>
+                    </Card.Header>
 
-                <Label for="team_number">Team Number</Label>
-                <Input id="team_number"type="text" placeholder="Team Number"/>
-            </Card.Content>
+                    <Card.Content class="flex flex-col gap-2">
+                        <Label for="username">Username</Label>
+                        <Input id="username" type="text" placeholder="Username"/>
 
-            <Card.Footer>
-                <Button><ArrowRight weight="bold" /> Continue</Button>
-            </Card.Footer>
-        </Card.Root>
+                        <Label for="team_number">Team Number</Label>
+                        <Input id="team_number"type="text" placeholder="Team Number"/>
+                    </Card.Content>
 
-        <Card.Root class="max-w-128 p-4 min-w-64">
-            <Card.Header>
-                <Card.Title>Authentication</Card.Title>
-                <Card.Description>Sign in or create an account to use Open Scouting with</Card.Description>
-            </Card.Header>
+                    <Card.Footer>
+                        <Button><ArrowRight weight="bold" /> Continue</Button>
+                    </Card.Footer>
+                </Card.Root>
+            </Tabs.Content>
+            
+            <Tabs.Content value="auth">
+                <Card.Root class="max-w-128 p-4 min-w-64">
+                    <Card.Header>
+                        <Card.Title>Authentication</Card.Title>
+                        <Card.Description>Sign in or create an account to use Open Scouting with</Card.Description>
+                    </Card.Header>
 
-            <Card.Content class="flex flex-col gap-2">
-                {#if user === null}
-                    <Skeleton class="w-1/2" />
-                {:else if user}
-                    <p class="text-lg">Signed in as <strong>{user.username}</strong></p>
+                    <Card.Content class="flex flex-col gap-2">
+                        {#if user === null}
+                            <Skeleton class="w-1/2" />
+                        {:else if user}
+                            <p class="text-lg">Signed in as <strong>{user.username}</strong></p>
 
-                    {#if organizations === null}
-                        <Skeleton class="w-1/2" />
-                    {:else if organizations.length === 0}
+                            {#if organizations === null}
+                                <Skeleton class="w-1/2" />
+                            {:else if organizations.length === 0}
 
-                    {:else}
-                        <div class="flex flex-row gap-2 items-center">
-                            <div class="flex flex-col gap-2">
-                                <p>Organization</p>
-                                <p class="text-muted-foreground text-sm">Organizations allow you to use custom match and pit scouting questions specific for your team</p>
-                            </div>
+                            {:else}
+                                <div class="flex flex-row gap-2 items-center">
+                                    <div class="flex flex-col gap-2">
+                                        <p>Organization</p>
+                                        <p class="text-muted-foreground text-sm">Organizations allow you to use custom match and pit scouting questions specific for your team</p>
+                                    </div>
 
-                            <Select.Root type="single" name="organization" id="organization" bind:value={organization_value}>
-                                <Select.Trigger>
-                                    {organization_value.label}
-                                </Select.Trigger>
-                                <Select.Content>
-                                    <Select.Label>Organizations</Select.Label>
-                                    <Select.Item value={{"name":"default", "id":"0", "label":"None"}} label="None" />
-                                    {#each organizations as organization}
-                                        <Select.Item value={{"name":organization.name, "id":organization.id, "label":organization.label}} label={organization.label} />
-                                    {/each}
-                                </Select.Content>
-                            </Select.Root>
-                        </div>
-                    {/if}
-                {:else}
-                    <p>Not signed in</p>
-                {/if}
-            </Card.Content>
+                                    <Select.Root type="single" name="organization" id="organization" bind:value={organization_value}>
+                                        <Select.Trigger>
+                                            {organization_value.label}
+                                        </Select.Trigger>
+                                        <Select.Content>
+                                            <Select.Label>Organizations</Select.Label>
+                                            <Select.Item value={{"name":"default", "id":"0", "label":"None"}} label="None" />
+                                            {#each organizations as organization}
+                                                <Select.Item value={{"name":organization.name, "id":organization.id, "label":organization.label}} label={organization.label} />
+                                            {/each}
+                                        </Select.Content>
+                                    </Select.Root>
+                                </div>
+                            {/if}
+                        {:else}
+                            <p>Not signed in</p>
+                        {/if}
+                    </Card.Content>
 
-            <Card.Footer class="flex flex-row gap-2">
-                {#if user}
-                    <Button variant="outline" onclick={async () => {await signOut(); window.location.reload()}}>
-                        <SignOut weight="bold" />
-                        Sign Out
-                    </Button>
-                    <Button>
-                        <Avatar.Root>
-                            <!-- TODO: Actually load avatar from user account -->
-                            <Avatar.Image src={`https://github.com/${user.username}.png`} alt={user.username} />
-                            <Avatar.Fallback>SC</Avatar.Fallback>
-                        </Avatar.Root>
-                        
-                        Continue as {user.username} <ArrowRight weight="bold" />
-                    </Button>
-                {:else}
-                    <Button href="/authentication">Sign In <SignIn weight="bold" /></Button>
-                {/if}
-            </Card.Footer>
-        </Card.Root>
+                    <Card.Footer class="flex flex-row gap-2">
+                        {#if user}
+                            <Button variant="outline" onclick={async () => {await signOut(); window.location.reload()}}>
+                                <SignOut weight="bold" />
+                                Sign Out
+                            </Button>
+                            <Button>
+                                <Avatar.Root>
+                                    <!-- TODO: Actually load avatar from user account -->
+                                    <Avatar.Image src={`https://github.com/${user.username}.png`} alt={user.username} />
+                                    <Avatar.Fallback>SC</Avatar.Fallback>
+                                </Avatar.Root>
+                                
+                                Continue as {user.username} <ArrowRight weight="bold" />
+                            </Button>
+                        {:else}
+                            <Button href="/authentication">Sign In <SignIn weight="bold" /></Button>
+                        {/if}
+                    </Card.Footer>
+                </Card.Root>
+            </Tabs.Content>
+        </Tabs.Root>
+        
+
+        
     </div>
 </div>
