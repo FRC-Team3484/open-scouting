cd4ba6a231dd94dcb1da28aaa7f95c663288f345
nfoert <nofoert@gmail.com>
2025-10-05 11:32:39 -0400

    feat(frontend): Show authentication status in the mobile navbar

--- frontend/src/lib/components/generic/NavBar.svelte
+++ frontend/src/lib/components/generic/NavBar.svelte
@@ -8,6 +8,7 @@
     import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
     import * as Drawer from "$lib/components/ui/drawer/index.js";
 	import { ArrowRight, List, X } from "phosphor-svelte";
+	import Separator from "../ui/separator/separator.svelte";
 
     let user = null;
 
@@ -72,6 +73,40 @@
                 <Logo text={false} style="tiny" href="/" />
                 <Drawer.Close><Button variant="outline" size="icon"><X weight="bold"/></Button></Drawer.Close>
             </div>
+            <div class="flex flex-row gap-4 items-center">
+                {#if user === null}
+                    <Skeleton class="h-8 w-8 rounded-full" />
+                {:else if user}
+                    <Button variant="outline" size="icon" class="!rounded-full">
+                        <DropdownMenu.Root>
+                            <DropdownMenu.Trigger>
+                                <Avatar.Root>
+                                    <!-- TODO: Actually load avatar from user account -->
+                                    <Avatar.Image src={`https://github.com/${user.username}.png`} alt={user.username} />
+                                    <Avatar.Fallback>{user.username}</Avatar.Fallback>
+                                </Avatar.Root>
+                            </DropdownMenu.Trigger>
+                            <DropdownMenu.Content class="w-56" align="start">
+                                <DropdownMenu.Label>{user.username}</DropdownMenu.Label>
+                                <DropdownMenu.Group>
+                                    <DropdownMenu.Item>
+                                        Profile
+                                    </DropdownMenu.Item>
+                                </DropdownMenu.Group>
+                                <DropdownMenu.Separator />
+                                <DropdownMenu.Item onclick={() => {signOut(); window.location.reload()}}>
+                                    Log out
+                                </DropdownMenu.Item>
+                            </DropdownMenu.Content>
+                        </DropdownMenu.Root>
+                    </Button>
+
+                    <p class="text-muted-foreground">{user.username}</p>
+                {:else}
+                    <Button variant="outline" href="/authentication" class="w-full">Login</Button>
+                {/if}
+            </div>
+            <Separator orientation="horizontal" />
             <Button variant="outline" href="/">Home</Button>
             <Button variant="outline" href="/" disabled>View Data</Button>
             <Button variant="outline" href="/" disabled>Events</Button>
