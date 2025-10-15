36f1a47904897e2a3689bcc50b22ade3c2442d23
nfoert <nofoert@gmail.com>
2025-10-15 16:25:54 -0400

    feat(frontend): Create base field manager dialogs

--- frontend/src/lib/components/generic/MatchScoutingFields.svelte
+++ frontend/src/lib/components/generic/MatchScoutingFields.svelte
@@ -2,6 +2,11 @@
 	import { apiFetch } from "$lib/utls/api";
 	import { onMount } from "svelte";
 	import Skeleton from "../ui/skeleton/skeleton.svelte";
+	import { FolderPlus, PlusCircle } from "phosphor-svelte";
+    import * as Card from "$lib/components/ui/card/index.js";
+    import * as Dialog from "$lib/components/ui/dialog/index.js";
+	import Button from "../ui/button/button.svelte";
+	import Separator from "../ui/separator/separator.svelte";
 
     let fields = [];
 
@@ -24,6 +29,48 @@
         <Skeleton class="w-128 h-32" />
         <Skeleton class="w-128 h-32" />
     {:else}
+        <Separator />
+
+        <Card.Root class="w-auto min-w-64">
+            <Card.Content>
+                <div class="flex flex-col gap-4">
+                    <Dialog.Root>
+                        <Dialog.Trigger>
+                            <Button><PlusCircle weight="bold" /> Add Field</Button>
+                        </Dialog.Trigger>
+
+                        <Dialog.Content>
+                            <Dialog.Header>
+                                <Dialog.Title>Add Field</Dialog.Title>
+                                <Dialog.Description>Create a new field</Dialog.Description>
+                            </Dialog.Header>
+
+                            <Dialog.Footer>
+                                <Button type="submit">Create</Button>
+                            </Dialog.Footer>
+                        </Dialog.Content>
+                    </Dialog.Root>
+
+                    <Dialog.Root>
+                        <Dialog.Trigger>
+                            <Button><FolderPlus weight="bold" /> Add Section</Button>
+                        </Dialog.Trigger>
+
+                        <Dialog.Content>
+                            <Dialog.Header>
+                                <Dialog.Title>Add Section</Dialog.Title>
+                                <Dialog.Description>Create a new section</Dialog.Description>
+                            </Dialog.Header>
+
+                            <Dialog.Footer>
+                                <Button type="submit">Create</Button>
+                            </Dialog.Footer>
+                        </Dialog.Content>
+                    </Dialog.Root>
+                </div>
+            </Card.Content>
+        </Card.Root>
+
         {#each fields as field}
             <p>field</p>
         {/each}
