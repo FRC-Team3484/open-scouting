930ceeee0d60c42c4a551c881bc7c395b49d7913
nfoert <nofoert@gmail.com>
2025-11-14 16:34:25 -0500

    fix: Properly space nexted fields and sections

--- frontend/src/lib/components/generic/fields/Section.svelte
+++ frontend/src/lib/components/generic/fields/Section.svelte
@@ -13,21 +13,23 @@
 </script>
 
 <BaseSection field={field} editable={editable} getFields={getFields}>
-	{#each field.fields as child (child.uuid)}
-		{#if child.field_type === "string"}
-			<StringField field={child} editable={editable} getFields={getFields} />
-		{:else if child.field_type === "large_number"}
-			<LargeNumberField field={child} editable={editable} getFields={getFields} />
-		{:else if child.field_type === "small_number"}
-			<SmallNumberField field={child} editable={editable} getFields={getFields} />
-		{:else if child.field_type === "boolean"}
-			<BooleanField field={child} editable={editable} getFields={getFields} />
-		{:else if child.field_type === "choice"}
-			<ChoiceField field={child} editable={editable} getFields={getFields} />
-		{:else if child.field_type === "multiple_choice"}
-			<MultipleChoiceField field={child} editable={editable} getFields={getFields} />
-		{:else if child.field_type === "section"}
-			<Section field={child} editable={editable} getFields={getFields} />
-		{/if}
-	{/each}
+	<div class="flex flex-col gap-4">
+		{#each field.fields as child (child.uuid)}
+			{#if child.field_type === "string"}
+				<StringField field={child} editable={editable} getFields={getFields} />
+			{:else if child.field_type === "large_number"}
+				<LargeNumberField field={child} editable={editable} getFields={getFields} />
+			{:else if child.field_type === "small_number"}
+				<SmallNumberField field={child} editable={editable} getFields={getFields} />
+			{:else if child.field_type === "boolean"}
+				<BooleanField field={child} editable={editable} getFields={getFields} />
+			{:else if child.field_type === "choice"}
+				<ChoiceField field={child} editable={editable} getFields={getFields} />
+			{:else if child.field_type === "multiple_choice"}
+				<MultipleChoiceField field={child} editable={editable} getFields={getFields} />
+			{:else if child.field_type === "section"}
+				<Section field={child} editable={editable} getFields={getFields} />
+			{/if}
+		{/each}
+	</div>
 </BaseSection>
