8eda11a74f883b614fca9b623998a3281a4c881d
nfoert <nofoert@gmail.com>
2025-11-30 14:01:12 -0500

    feat(frontend/db): Add season and event data to the local db specification

--- frontend/src/lib/utls/db.ts
+++ frontend/src/lib/utls/db.ts
@@ -2,15 +2,21 @@ import Dexie from 'dexie';
 
 export class OpenScoutingDB extends Dexie {
   match_scouting! : Dexie.Table<{uuid: string, data: any, synced: boolean}, string>;
+  season_data! : Dexie.Table<{year: number, fields: any, game_pieces: any}, number>;
+  event! : Dexie.Table<{uuid: string, year: number, event_code: string, name: string, type: string, city: string, country: string, start_date: string, end_date: string}, string>;
 
   constructor() {
     super('open-scouting');
     
     this.version(1).stores({
-      match_scouting: "&uuid, data, synced"
+      match_scouting: "&uuid, data, synced",
+      season_data: "&year, fields, game_pieces",
+      event: "&uuid, year, event_code, name, type, city, country, start_date, end_date"
     });
 
     this.match_scouting = this.table('match_scouting');
+    this.season_data = this.table('season_data');
+    this.event = this.table('event');
   }
 }
 
