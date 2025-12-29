82aa1975721c2f2eb457ef4463c77a5c2ec983d4
nfoert <nofoert@gmail.com>
2025-12-29 16:46:55 -0500

    feat(frontend): Add FilterList component for the event and team filters on the data page

---
+++ frontend/src/lib/components/data/FilterList.svelte
@@ -0,0 +1,101 @@
+<script lang="ts">
+    import * as Popover from "$lib/components/ui/popover/index.js";
+    import * as Drawer from "$lib/components/ui/drawer/index.js";
+	import { onMount } from "svelte";
+	import Button from "../ui/button/button.svelte";
+	import { PlusCircle, Trash, X, XCircle } from "phosphor-svelte";
+	import Input from "../ui/input/input.svelte";
+	import Separator from "../ui/separator/separator.svelte";
+	import { browser } from "$app/environment";
+
+    let { filterTitle, values, labels, selected = $bindable([]) } = $props();
+
+    let open = $state(false);
+    let search = $state("");
+    let isDesktop = $state(false);
+
+    let filteredValues = $derived(
+        values.filter((value) => !selected.includes(value))
+            .filter((value) => labels[values.indexOf(value)].toLowerCase().includes(search.toLowerCase()) || value.toLowerCase().includes(search.toLowerCase()))
+    )
+
+    function addItem(value) {
+        selected = [...selected, value];
+        open = false;
+        search = "";
+    }
+
+    function deleteItem(value) {
+        selected = selected.filter((item) => item !== value);
+        open = false;
+        search = "";
+    }
+
+    function checkScreenSize() {
+        // tailwind-css' md breakpoint is 768 pixels
+        isDesktop = window.innerWidth >= 768;
+    }
+
+    onMount(() => {
+        if (browser) {
+            checkScreenSize();
+            window.addEventListener("resize", checkScreenSize);
+            return () => window.removeEventListener("resize", checkScreenSize);
+        }
+    })
+</script>
+
+{#snippet filters()}
+    <div class="flex flex-col gap-2 overflow-y-scroll">
+        <div class="flex flex-row gap-2 items-center justify-between">
+            <p class="font-bold">{filterTitle}</p>
+            <Button size="icon-sm" variant="outline" onclick={() => open = false}><XCircle weight="bold" /></Button>
+        </div>
+
+        <div class="flex flex-row gap-2">
+            <Input type="text" placeholder="Search..." bind:value={search} />
+            <Button size="icon-sm" variant="outline" onclick={() => search = ""} ><Trash weight="bold" /></Button>
+        </div>
+
+        <Separator orientation="horizontal" />
+
+        {#each filteredValues as value}
+            <div class="flex flex-row gap-2 items-center">
+                <Button variant="ghost" onclick={() => addItem(value)}>{labels[values.indexOf(value)]}</Button>
+            </div>
+        {/each}
+
+        {#if filteredValues.length < 0}
+            <p class="text-muted-foreground">No matches found.</p>
+        {/if}
+    </div>
+{/snippet}
+
+<div class="flex flex-row gap-2 max-h-screen max-w-screen flex-wrap items-center">
+    {#if isDesktop}
+        <Popover.Root bind:open>
+            <Popover.Trigger>
+                <Button variant="outline"><PlusCircle weight="bold" /> Add</Button>
+            </Popover.Trigger>
+
+            <Popover.Content>
+                {@render filters()}
+            </Popover.Content>
+        </Popover.Root>
+    {:else}
+        <Drawer.Root bind:open>
+            <Drawer.Trigger>
+                <Button variant="outline"><PlusCircle weight="bold" /> Add</Button>
+            </Drawer.Trigger>
+
+            <Drawer.Content class="p-4">
+                {@render filters()}
+            </Drawer.Content>
+        </Drawer.Root>
+    {/if}
+    
+    {#each selected as value}
+        <Button variant="outline" size="sm" onclick={() => deleteItem(value)} ><X weight="bold" /> {labels[values.indexOf(value)]}</Button>
+    {/each}
+</div>
+
--- frontend/src/lib/components/data/Filters.svelte
+++ frontend/src/lib/components/data/Filters.svelte
@@ -4,6 +4,7 @@
 	import { apiFetch } from "$lib/utils/api";
 	import { Buildings, Calendar, Faders, Users } from "phosphor-svelte";
 	import { onMount } from "svelte";
+	import FilterList from "./FilterList.svelte";
     
     let { filters = $bindable() } = $props();
 
@@ -21,8 +22,23 @@
         }));
 
         filters.season = seasons[0].value;
-    }
 
+        let filterQueryParams = new URLSearchParams(window.location.search);
+
+        filterQueryParams.set("year", filters.season);
+        if (filters.event_codes.length > 0) {
+            filterQueryParams.set("event_codes", filters.event_codes.join(","));
+        }
+        if (filters.team_numbers.length > 0) {
+            filterQueryParams.set("team_numbers", filters.team_numbers.join(","));
+        }
+
+        const filtersRequest = await apiFetch(`/data/filters?${filterQueryParams.toString()}`);
+        console.log(filtersRequest);
+
+        events = filtersRequest.events;
+        teams = filtersRequest.teams;
+    }
 
     onMount(async () => {
         getFilterOptions();
@@ -37,7 +53,7 @@
                 <p class="text-lg font-bold"> Filters</p>
             </div>
 
-            <p class="text-muted-foreground">Filter the loaded match scouting data by year, events, and teams.</p>
+            <p class="text-muted-foreground text-left">Filter the loaded match scouting data by year, events, and teams.</p>
 
             <div class="flex flex-col gap-2">
                 <div class="flex flex-col gap-2">
@@ -62,6 +78,8 @@
                         <Buildings weight="bold" size={16} />
                         <p>Events</p>
                     </div>
+
+                    <FilterList filterTitle="Add Event Filter" values={events.map((e) => e.event_code)} labels={events.map((e) => e.event_name)} bind:selected={filters.event_codes} />
                 </div>
 
                 <div class="flex flex-col gap-2">
@@ -69,6 +87,8 @@
                         <Users weight="bold" size={16} />
                         <p>Teams</p>
                     </div> 
+
+                    <FilterList filterTitle="Add Team Filter" values={teams.map((t) => t.team_number.toString())} labels={teams.map((t) => t.team_number.toString())} bind:selected={filters.team_numbers} />
                 </div>
             </div>
         </div>
