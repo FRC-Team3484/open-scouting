a2513191b52386d1699eff870291f86882139fed
nfoert <nofoert@gmail.com>
2026-01-02 07:35:16 -0500

    feat(frontend): Render other data on the data page

--- frontend/src/lib/components/data/TeamOthers.svelte
+++ frontend/src/lib/components/data/TeamOthers.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import * as Card from "$lib/components/ui/card/index.js";
 	import BaseTeamFolder from "./BaseTeamFolder.svelte";
     
     let { others } = $props();
@@ -6,9 +7,17 @@
 
 <BaseTeamFolder title="Other">
     <div class="flex flex-col gap-2 text-left">
-        <p class="font-bold text-lg">Other</p>
         {#each others as other}
-            <p class="text-sm">{other.field_name}</p>
+            <Card.Root>
+                <Card.Content>
+                    <p class="font-bold">{other.field_name}</p>
+                    <ul class="list-disc list-inside">
+                        {#each other.values as value}
+                            <li class="text-sm">Match {value.match_number} - {value.value}</li>
+                        {/each}
+                    </ul>
+                </Card.Content>
+            </Card.Root>
         {/each}
     </div>
 </BaseTeamFolder>
