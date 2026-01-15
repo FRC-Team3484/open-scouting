46fade0232114954ad98722ea448219e05f38895
nfoert <nofoert@gmail.com>
2026-01-15 07:54:30 -0500

    feat(backend): Add functions to get and create custom events

--- backend/app/main.py
+++ backend/app/main.py
@@ -1104,3 +1104,44 @@ async def get_data(
         result.append(team)
 
     return result
+
+# Custom Events
+@app.get("/event/custom/{season_uuid}")
+def get_custom_events(season_uuid: str):
+    season = Season.get_or_none(uuid=season_uuid)
+    if not season:
+        raise HTTPException(status_code=404, detail="Season not found")
+
+    return Event.filter(season=season, custom=True)
+
+@app.post("/event/custom/{season_uuid}/create")
+def create_custom_event(
+    season_uuid: str,
+
+    event_code: str = Form(...),
+    event_name: str = Form(...),
+    event_type: str = Form(...),
+    event_city: str = Form(...),
+    event_country: str = Form(...),
+    event_start_date: str = Form(...),
+    event_end_date: str = Form(...),
+    
+    ):
+    
+    season = Season.get_or_none(uuid=season_uuid)
+    if not season:
+        raise HTTPException(status_code=404, detail="Season not found")
+
+    event, _ = Event.get_or_create(
+        season=season,
+        event_code=event_code,
+        name=event_name,
+        type=event_type,
+        city=event_city,
+        country=event_country,
+        start_date=event_start_date,
+        end_date=event_end_date,
+        custom=True
+    )
+
+    return event
