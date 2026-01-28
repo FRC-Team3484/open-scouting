8364227a631b66de459eb1cae6731c95fb6607c9
nfoert <nofoert@gmail.com>
2026-01-28 14:19:12 -0500

    feat(backend): Create schemas for event routes

--- backend/app/routes/event.py
+++ backend/app/routes/event.py
@@ -1,45 +1,36 @@
-from fastapi import APIRouter, Form, HTTPException
+from fastapi import APIRouter, HTTPException
 
 from ..models import Event, Season
-
+from ..schemas.event import CustomEventsRequest, EventResponse, CustomEventRequest
 
 router: APIRouter = APIRouter()
 
-@router.get("/event/custom/{season_uuid}")
-async def get_custom_events(season_uuid: str):
-    season = await Season.get_or_none(uuid=season_uuid)
+@router.get("/event/custom/{season_uuid}", response_model=list[EventResponse])
+async def get_custom_events(data: CustomEventsRequest) -> list[Event]:
+    season: Season | None = await Season.get_or_none(uuid=data.season_uuid)
     if not season:
         raise HTTPException(status_code=404, detail="Season not found")
 
     return await Event.filter(season=season, custom=True)
 
-@router.post("/event/custom/{season_uuid}/create")
+@router.post("/event/custom/{season_uuid}/create", response_model=EventResponse)
 async def create_custom_event(
-    season_uuid: str,
-
-    event_code: str = Form(...),
-    event_name: str = Form(...),
-    event_type: str = Form(...),
-    event_city: str = Form(...),
-    event_country: str = Form(...),
-    event_start_date: str = Form(...),
-    event_end_date: str = Form(...)
+        data: CustomEventRequest
+    ) -> Event:
     
-    ):
-    
-    season = await Season.get_or_none(uuid=season_uuid)
+    season: Season | None = await Season.get_or_none(uuid=data.season_uuid)
     if not season:
         raise HTTPException(status_code=404, detail="Season not found")
 
     event, _ = await Event.get_or_create(
         season=season,
-        event_code=event_code,
-        name=event_name,
-        type=event_type,
-        city=event_city,
-        country=event_country,
-        start_date=event_start_date,
-        end_date=event_end_date,
+        event_code=data.event_code,
+        name=data.event_name,
+        type=data.event_type,
+        city=data.event_city,
+        country=data.event_country,
+        start_date=data.event_start_date,
+        end_date=data.event_end_date,
         custom=True
     )
 
---
+++ backend/app/schemas/event.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from uuid import UUID
+from pydantic import BaseModel
+
+
+class CustomEventsRequest(BaseModel):
+    season_uuid: UUID
+
+class EventResponse(BaseModel):
+    uuid: UUID
+    season: UUID
+    event_code: str
+    name: str
+    type: str
+    city: str
+    country: str
+    start_date: str
+    end_date: str
+    pits_generated: bool
+    custom: bool
+    created_at: datetime
+
+class CustomEventRequest(BaseModel):
+    season_uuid: UUID
+    event_code: str
+    event_name: str
+    event_type: str
+    event_city: str
+    event_country: str
+    event_start_date: str
+    event_end_date: str
