f8d040da43bb2ecf5bac2e5a612a2d9000dc57da
nfoert <nofoert@gmail.com>
2026-01-28 14:12:56 -0500

    feat(backend): Create schemas for pit scouting routes

--- backend/app/routes/pit_scouting.py
+++ backend/app/routes/pit_scouting.py
@@ -1,3 +1,6 @@
+from backend.app.models import Organization, PitScoutingField, Season
+
+
 import json
 import os
 import httpx
@@ -6,146 +9,123 @@ from fastapi import APIRouter, Depends, Form, HTTPException
 
 from ..dependencies import get_current_user
 from ..models import Event, Organization, PitScoutingAnswer, PitScoutingField, Season, TeamPit, User
-
+from ..schemas.pit_scouting import EditPitFieldRequest, PitFieldsRequest, PitFieldResponse, MessageResponse, CreatePitFieldRequest, DeletePitFieldRequest, GetPitsForSeasonRequest, SubmitPitFieldAnswerRequest
 
 router: APIRouter = APIRouter()
 
 TBA_API_KEY = os.getenv("TBA_API_KEY")
 
 
-@router.get("/pits/fields/{season_uuid}")
-async def get_pit_fields(season_uuid: str):
-    season = await Season.get_or_none(uuid=season_uuid)
+@router.get("/pits/fields/{season_uuid}", response_model=list[PitFieldResponse])
+async def get_pit_fields(data: PitFieldsRequest) -> list[PitScoutingField]:
+    season: Season | None = await Season.get_or_none(uuid=data.season_uuid)
 
     if not season:
         raise HTTPException(status_code=404, detail="Season not found")
 
-    fields = await PitScoutingField.filter(season=season)
+    fields: list[PitScoutingField] = await PitScoutingField.filter(season=season)
     return fields
 
-@router.delete("/pits/fields/{season_uuid}/clear")
-async def clear_pit_fields(season_uuid: str, current_user: User = Depends(get_current_user)):
-    season = await Season.get_or_none(uuid=season_uuid)
+@router.delete("/pits/fields/{season_uuid}/clear", response_model=MessageResponse)
+async def clear_pit_fields(data: PitFieldsRequest, current_user: User = Depends(get_current_user)):
+    season: Season | None = await Season.get_or_none(uuid=data.season_uuid)
     if not season:
         raise HTTPException(status_code=404, detail="Season not found")
     await PitScoutingField.filter(season=season).delete()
     return {"message": "Fields cleared"}
 
-@router.post("/pits/fields/{season_uuid}/create")
+@router.post("/pits/fields/{season_uuid}/create", response_model=PitFieldResponse)
 async def create_pit_field(
-    season_uuid: str,
+        data: CreatePitFieldRequest
+    ) -> PitScoutingField:
 
-    name: str = Form(...),
-    field_type: str = Form(...),
-    options: str = Form(...),
-    order: int = Form(...),
-    organization_uuid: str = Form(...)
-    ):
-
-    print(options)
-
-    season = await Season.get_or_none(uuid=season_uuid)
+    season: Season | None = await Season.get_or_none(uuid=data.season_uuid)
     if not season:
         raise HTTPException(status_code=404, detail="Season not found")
 
-    if organization_uuid != "":
-        organization = await Organization.get_or_none(uuid=organization_uuid)
+    if data.organization_uuid != "":
+        organization: Organization | None = await Organization.get_or_none(uuid=data.organization_uuid)
         if not organization:
             raise HTTPException(status_code=404, detail="Organization not found")
     else:
         organization = None
 
-    field = await PitScoutingField.create(
+    field: PitScoutingField = await PitScoutingField.create(
         season=season,
-        name=name,
-        field_type=field_type,
-        options=options,
-        order=order,
+        name=data.name,
+        field_type=data.field_type,
+        options=data.options,
+        order=data.order,
         organization=organization
     )
     return field
 
-@router.patch("/pits/fields/{season_uuid}/edit/{field_uuid}")
+@router.patch("/pits/fields/{season_uuid}/edit/{field_uuid}", response_model=PitFieldResponse)
 async def edit_pit_field(
-    season_uuid: str, 
-    field_uuid: str,
-
-    name: str = Form(...),
-    field_type: str = Form(...),
-    options: str = Form(...),
-    order: int = Form(...),
-    organization_uuid: str = Form(...)
+        data: EditPitFieldRequest
+    ) -> PitScoutingField:
     
-    ):
-    
-    season = await Season.get_or_none(uuid=season_uuid)
+    season: Season | None = await Season.get_or_none(uuid=data.season_uuid)
     if not season:
         raise HTTPException(status_code=404, detail="Season not found")
 
-    field = await PitScoutingField.get_or_none(uuid=field_uuid)
+    field: PitScoutingField | None = await PitScoutingField.get_or_none(uuid=data.field_uuid)
     if not field:
         raise HTTPException(status_code=404, detail="Field not found")
 
-    if organization_uuid != "":
-        organization = await Organization.get_or_none(uuid=organization_uuid)
+    if data.organization_uuid != "":
+        organization: Organization | None = await Organization.get_or_none(uuid=data.organization_uuid)
         if not organization:
             raise HTTPException(status_code=404, detail="Organization not found")
     else:
         organization = None
 
-    field.name = name
-    field.field_type = field_type
-    field.options = options
-    field.order = order
+    field.name = data.name
+    field.field_type = data.field_type
+    field.options = data.options
+    field.order = data.order
     field.organization = organization
 
     await field.save()
     return field
 
-@router.delete("/pits/fields/{field_uuid}/delete")
+@router.delete("/pits/fields/{field_uuid}/delete", response_model=PitFieldResponse)
 async def delete_pit_field(
-        field_uuid: str, 
+        data: DeletePitFieldRequest,
         current_user: User = Depends(get_current_user)
-    ):
+    ) -> PitScoutingField:
 
-    field = await PitScoutingField.get_or_none(uuid=field_uuid)
+    field: PitScoutingField | None = await PitScoutingField.get_or_none(uuid=data.field_uuid)
     if not field:
         raise HTTPException(status_code=404, detail="Field not found")
 
     await field.delete()
     return field
 
+# TODO: This needs a proper response_model
 @router.post("/pits/get/{season_uuid}")
 async def get_pits(
-    season_uuid: str, 
-    
-    event_code: str = Form(...),
-    event_name: str = Form(...),
-    event_type: str = Form(...),
-    event_city: str = Form(...),
-    event_country: str = Form(...),
-    event_start_date: str = Form(...),
-    event_end_date: str = Form(...)
+        data: GetPitsForSeasonRequest
     ):
     
-    season = await Season.get_or_none(uuid=season_uuid)
+    season: Season | None = await Season.get_or_none(uuid=data.season_uuid)
     if not season:
         raise HTTPException(status_code=404, detail="Season not found")
 
     event, _ = await Event.get_or_create(
         season=season,
-        event_code=event_code,
-        name=event_name,
-        type=event_type,
-        city=event_city,
-        country=event_country,
-        start_date=event_start_date,
-        end_date=event_end_date
+        event_code=data.event_code,
+        name=data.event_name,
+        type=data.event_type,
+        city=data.event_city,
+        country=data.event_country,
+        start_date=data.event_start_date,
+        end_date=data.event_end_date
     )
 
     # If pits have not been generated yet, get teams from TBA and create TeamPits
     if not event.pits_generated and TBA_API_KEY != "" and TBA_API_KEY is not None and event.custom is False:
-        event_key = str(season.year) + event_code
+        event_key = str(season.year) + data.event_typeevent_code
         async with httpx.AsyncClient(timeout=10.0) as client:
             response = await client.get(
                 f"https://www.thebluealliance.com/api/v3/event/{event_key}/teams",
@@ -186,21 +166,9 @@ async def get_pits(
         for pit in pits
     ]
 
-@router.post("/pits/submit/{season_uuid}/{team_number}")
+@router.post("/pits/submit/{season_uuid}/{team_number}", response_model=MessageResponse)
 async def submit_pit(
-    season_uuid: str, 
-    team_number: int,
-
-    event_code: str = Form(...),
-    event_name: str = Form(...),
-    event_type: str = Form(...),
-    event_city: str = Form(...),
-    event_country: str = Form(...),
-    event_start_date: str = Form(...),
-    event_end_date: str = Form(...),
-
-    answers: str = Form(...),
-    nickname: str = Form(...)
+        data: SubmitPitFieldAnswerRequest
     ):
     """
     Get the season and event from the uuids. Then, check if a pit with that team number exists.
@@ -211,25 +179,25 @@ async def submit_pit(
     If a pit does not exist, that means it was created by the client of a user. It should be created.
     """
     
-    season = await Season.get_or_none(uuid=season_uuid)
+    season: Season | None = await Season.get_or_none(uuid=data.season_uuid)
     if not season:
         raise HTTPException(status_code=404, detail="Season not found")
 
-    event = await Event.get_or_none(event_code=event_code, season=season).first()
+    event = await Event.get_or_none(event_code=data.event_code, season=season).first()
     if not event:
         raise HTTPException(status_code=404, detail="Event not found")
 
     pit, created = await TeamPit.get_or_create(
-        team_number=team_number,
+        team_number=data.team_number,
         season=season,
         event=event,
-        nickname=nickname
+        nickname=data.nickname
     )
 
     if created:
-        print("Created pit", pit.uuid, "for team", team_number, "and event", event.uuid)
+        print("Created pit", pit.uuid, "for team", data.team_number, "and event", event.uuid)
 
-    for answer in json.loads(answers):
+    for answer in json.loads(data.answers):
         field = await PitScoutingField.get_or_none(uuid=answer["field_uuid"])
 
         if not field:
@@ -246,4 +214,4 @@ async def submit_pit(
         if created:
             print("Created answer", answer["uuid"], "for pit", pit.uuid, "and field", field.uuid)
 
-    return
+    return {"message": "Pit submitted successfully"}
---
+++ backend/app/schemas/pit_scouting.py
@@ -0,0 +1,61 @@
+
+from datetime import datetime
+from typing import Any
+from uuid import UUID
+from pydantic import BaseModel
+
+
+class PitFieldsRequest(BaseModel):
+    season_uuid: UUID
+
+class PitFieldResponse(BaseModel):
+    uuid: UUID
+    season: UUID
+    name: str
+    field_type: str
+    options: str
+    order: int
+    organization: UUID
+    created_at: datetime
+
+class MessageResponse(BaseModel):
+    message: str
+
+class CreatePitFieldRequest(BaseModel):
+    season_uuid: UUID
+    name: str
+    field_type: str
+    options: str
+    order: int
+    organization_uuid: UUID
+
+class EditPitFieldRequest(CreatePitFieldRequest):
+    field_uuid: UUID
+
+class DeletePitFieldRequest(BaseModel):
+    field_uuid: UUID
+
+class GetPitsForSeasonRequest(BaseModel):
+    season_uuid: UUID
+    event_code: str
+    event_name: str
+    event_type: str
+    event_city: str
+    event_country: str
+    event_start_date: str
+    event_end_date: str
+
+class SubmitPitFieldAnswerRequest(BaseModel):
+    season_uuid: UUID
+    team_number: int
+    
+    event_code: str
+    event_name: str
+    event_type: str
+    event_city: str
+    event_country: str
+    event_start_date: str
+    event_end_date: str
+
+    answers: dict[Any]
+    nickname: str
