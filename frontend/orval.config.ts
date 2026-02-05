cf394b97e478fd193d85881f5f9ba78faf42c786
nfoert <nofoert@gmail.com>
2026-02-05 16:41:23 -0500

    feat(frontend): Create zod types with orval

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -7,13 +7,25 @@ export default defineConfig({
             target: 'src/lib/api/api.ts',
             schemas: 'src/lib/api/model',
             client: 'fetch',
-            mock: true,
+            mock: false,
             override: {
                 mutator: {
                     path: 'src/lib/api/client.ts',
                     name: 'customInstance',
                 }
             }
+        },
+        input: {
+            target: 'http://localhost:8000/openapi.json',
+        },
+    },
+    zod: {
+        output: {
+            mode: 'tags-split',
+            target: 'src/lib/zod/zod.ts',
+            schemas: 'src/lib/zod/model',
+            client: 'zod',
+            mock: false,
         },
         input: {
             target: 'http://localhost:8000/openapi.json',
