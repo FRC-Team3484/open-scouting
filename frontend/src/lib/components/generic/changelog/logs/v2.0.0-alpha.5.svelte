fc77ceaa4f8fa66fb86be0eda11f06dd68cda3f4
nfoert <nofoert@gmail.com>
2026-02-17 22:43:50 -0500

    feat(frontend): Create initial changelog for v2.0.0-alpha.5

---
+++ frontend/src/lib/components/generic/changelog/logs/v2.0.0-alpha.5.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { ChartBar, Export, List } from "phosphor-svelte";
+	import MajorFeature from "../MajorFeature.svelte";
+	import Generic from "./Generic.svelte";
+</script>
+
+{#snippet summary()}
+    <p>This release adds several new features to the data page, and adds a new match scouting field for scouting Fuel</p>
+{/snippet}
+
+{#snippet features()}
+    <MajorFeature text="Changelog System">
+        {#snippet icon()}
+            <List weight="bold" />
+        {/snippet}
+    </MajorFeature>
+    
+    <MajorFeature text="Data Page Team Summaries">
+        {#snippet icon()}
+            <ChartBar weight="bold" />
+        {/snippet}
+    </MajorFeature>
+
+    <MajorFeature text="Match Scouting Data Export">
+        {#snippet icon()}
+            <Export weight="bold" />
+        {/snippet}
+    </MajorFeature>
+
+    <MajorFeature text="Copy Links to Events">
+        {#snippet icon()}
+            <ChartBar weight="bold" />
+        {/snippet}
+    </MajorFeature>
+
+    <MajorFeature text="New Number Field for Fuel">
+        {#snippet icon()}
+            <ChartBar weight="bold" />
+        {/snippet}
+    </MajorFeature>
+{/snippet}
+
+{#snippet details()}
+
+{/snippet}
+
+<Generic summary={summary} features={features} details={details} />
