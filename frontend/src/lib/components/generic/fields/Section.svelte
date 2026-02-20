643cc464a0155345c5bc2f9e7eb202b21b16b038
nfoert <nofoert@gmail.com>
2026-02-20 15:12:24 -0500

    feat(frontend): Be able to reorder match scouting fields

--- frontend/src/lib/components/generic/fields/Section.svelte
+++ frontend/src/lib/components/generic/fields/Section.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { flip } from "svelte/animate";
 	import BaseSection from "./BaseSection.svelte";
 
 	import StringField from "./StringField.svelte";
@@ -9,30 +10,52 @@
 	import MultipleChoiceField from "./MultipleChoiceField.svelte";
 	import Section from "./Section.svelte";
 	import CoarseSmallNumberField from "./CoarseSmallNumberField.svelte";
+	import { dragHandleZone } from "svelte-dnd-action";
 
-	let { field, editable, getFields } = $props();
+	let { field = $bindable(), editable, getFields, updateOrders } = $props();
+
+	function normalizeOrders(items) {
+        return items.map((item, index) => ({
+            ...item,
+            order: index,
+            fields: Array.isArray(item.fields)
+                ? normalizeOrders(item.fields)
+                : item.fields
+        }));
+    }
+
+	function onConsider(e: any) {
+		field.fields = e.detail.items;
+	}
+
+	async function onFinalize(e: any) {
+		field.fields = normalizeOrders(e.detail.items);
+		await updateOrders();
+	}
 </script>
 
 <BaseSection field={field} editable={editable} getFields={getFields}>
-	<div class="flex flex-col gap-4">
+	<div class="flex flex-col gap-4" use:dragHandleZone={{items: field.fields, flipDurationMs: 100}} onconsider={onConsider} onfinalize={onFinalize}>
 		{#each field.fields as child (child.uuid)}
-			{#if child.field_type === "string"}
-				<StringField field={child} editable={editable} getFields={getFields} />
-			{:else if child.field_type === "large_number"}
-				<LargeNumberField field={child} editable={editable} getFields={getFields} />
-			{:else if child.field_type === "small_number"}
-				<SmallNumberField field={child} editable={editable} getFields={getFields} />
-			{:else if child.field_type === "coarse_small_number"}
-				<CoarseSmallNumberField field={child} editable={editable} getFields={getFields} />
-			{:else if child.field_type === "boolean"}
-				<BooleanField field={child} editable={editable} getFields={getFields} />
-			{:else if child.field_type === "choice"}
-				<ChoiceField field={child} editable={editable} getFields={getFields} />
-			{:else if child.field_type === "multiple_choice"}
-				<MultipleChoiceField field={child} editable={editable} getFields={getFields} />
-			{:else if child.field_type === "section"}
-				<Section field={child} editable={editable} getFields={getFields} />
-			{/if}
+			<div animate:flip={{duration: 100}}>
+				{#if child.field_type === "string"}
+					<StringField field={child} editable={editable} getFields={getFields} />
+				{:else if child.field_type === "large_number"}
+					<LargeNumberField field={child} editable={editable} getFields={getFields} />
+				{:else if child.field_type === "small_number"}
+					<SmallNumberField field={child} editable={editable} getFields={getFields} />
+				{:else if child.field_type === "coarse_small_number"}
+					<CoarseSmallNumberField field={child} editable={editable} getFields={getFields} />
+				{:else if child.field_type === "boolean"}
+					<BooleanField field={child} editable={editable} getFields={getFields} />
+				{:else if child.field_type === "choice"}
+					<ChoiceField field={child} editable={editable} getFields={getFields} />
+				{:else if child.field_type === "multiple_choice"}
+					<MultipleChoiceField field={child} editable={editable} getFields={getFields} />
+				{:else if child.field_type === "section"}
+					<Section field={child} editable={editable} getFields={getFields} updateOrders={updateOrders} />
+				{/if}
+			</div>
 		{/each}
 	</div>
 </BaseSection>
