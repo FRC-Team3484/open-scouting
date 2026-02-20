8e21a644d0ec64f89847980efc361c65eeb85968
nfoert <nofoert@gmail.com>
2026-02-20 16:28:39 -0500

    feat(frontend): Update changelog for v2.0.0-alpha.5

--- frontend/src/lib/components/generic/changelog/logs/v2.0.0-alpha.5.svelte
+++ frontend/src/lib/components/generic/changelog/logs/v2.0.0-alpha.5.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { ChartBar, Export, List } from "phosphor-svelte";
+	import { ArrowsOutLineVertical, Bug, ChartBar, Export, List } from "phosphor-svelte";
 	import MajorFeature from "../MajorFeature.svelte";
 	import Generic from "./Generic.svelte";
 </script>
@@ -38,10 +38,28 @@
             <ChartBar weight="bold" />
         {/snippet}
     </MajorFeature>
+
+    <MajorFeature text="Reorder Match Scouting and Pit Questions">
+        {#snippet icon()}
+            <ArrowsOutLineVertical weight="bold" />
+        {/snippet}
+    </MajorFeature>
+
+    <MajorFeature text="Bug Fixes">
+        {#snippet icon()}
+            <Bug weight="bold" />
+        {/snippet}
+    </MajorFeature>
 {/snippet}
 
 {#snippet details()}
-
+    <ul class="list-disc list-inside ml-4">
+        <li>Accessing a link from another user's start page will automatically fill in the event and year details</li>
+        <li>Fixed an issue where pit scouting would create a duplicate pit on the client</li>
+        <li>Enabled the button to go to the data page in the nav bar</li>
+        <li>Team number autofilling is now disabled when an event has no valid match schedule data from TBA</li>
+        <li>Fixed an issue where rapidly clicking increment buttons in match scouting would zoom the page</li>
+    </ul>
 {/snippet}
 
 <Generic summary={summary} features={features} details={details} />
