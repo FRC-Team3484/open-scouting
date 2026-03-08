e0291dacac6ccb48e606ea542cc48d09a8ef2e92
nfoert <nofoert@gmail.com>
2026-03-08 12:02:05 -0400

    build: Try again

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running Aerich migrations..."
-aerich -t app.main.TORTOISE_ORM upgrade
+aerich -c pyproject.toml upgrade
 
 echo "Starting FastAPI..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
