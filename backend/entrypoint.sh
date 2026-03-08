e9987918d05e2cea6fab36be8c08e3df1acc5540
nfoert <nofoert@gmail.com>
2026-03-08 11:57:28 -0400

    build: try to fix aerich again

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,8 +2,7 @@
 set -e
 
 echo "Running Aerich migrations..."
-aerich init-db
-aerich upgrade
+aerich -t app.main.TORTOISE_ORM upgrade
 
 echo "Starting FastAPI..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
