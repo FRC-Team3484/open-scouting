2f42df440b2429c9d159f7d347221571a7ac0094
nfoert <nofoert@gmail.com>
2026-03-09 15:28:17 -0400

    chore: Add changelog file for v2.0.1

---
+++ frontend/src/lib/components/generic/changelog/logs/v2.0.1.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Generic from "./Generic.svelte";
+</script>
+
+{#snippet summary()}
+    <p>This release fixes issues with match scouting presets and game piece creation</p>
+{/snippet}
+
+{#snippet features()}
+
+{/snippet}
+
+{#snippet details()}    
+    <ul class="list-disc list-inside ml-4">
+        <li>Switched to use tortoise-orm's own migration system instead of aerich</li>
+        <li>Fixed an issue where game pieces couldn't be created</li>
+        <li>Fixed an issue where match scouting presets for 2024 and 2025 couldn't be imported due to the description field not being added</li>
+    </ul>
+{/snippet}
+
+<Generic summary={summary} features={features} details={details} />
