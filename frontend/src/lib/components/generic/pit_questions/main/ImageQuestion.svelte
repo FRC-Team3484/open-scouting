77fedca4caccf23576509ba732572528f92c458c
nfoert <nofoert@gmail.com>
2026-03-11 12:36:34 -0400

    fix(frontend): Pit scouting images are squished

--- frontend/src/lib/components/generic/pit_questions/main/ImageQuestion.svelte
+++ frontend/src/lib/components/generic/pit_questions/main/ImageQuestion.svelte
@@ -109,7 +109,7 @@
                     <div class="flex flex-col gap-2">
                         {#each answers as answer}
                             <div class="flex flex-col flex-wrap text-left">
-                                <img src={answer.value} class="w-128 aspect-square rounded-md bg-accent p-1" onerror={() => console.log("failed to load image")}>
+                                <img src={answer.value} class="w-128 rounded-md bg-accent p-1" onerror={() => console.log("failed to load image")}>
                                 <div class="flex flex-row flex-wrap items-center">
                                     <ArrowBendDownRight weight="bold" class="text-muted-foreground ml-4 mr-1"/>
                                     <User weight="bold" class="text-muted-foreground ml-2 mr-1"/>
