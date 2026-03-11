593c058c9b1d9fc113086f8e2f6c27992b687920
nfoert <nofoert@gmail.com>
2026-03-11 12:02:36 -0400

    feat(frontend): Make changelog file for v2.0.2

---
+++ frontend/src/lib/components/generic/changelog/logs/v2.0.2.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Generic from "./Generic.svelte";
+</script>
+
+{#snippet summary()}
+    <p>This release fixes various bugs and issues</p>
+{/snippet}
+
+{#snippet features()}
+
+{/snippet}
+
+{#snippet details()}    
+    <ul class="list-disc list-inside ml-4">
+        <li>Fixed an issue where the server would try to generate pits from TBA for custom events</li>
+        <li>Made the menu smaller on larger screens</li>
+        <li>Fixed an issue where multiple choice fields wouldn't reset</li>
+        <li>Adjust the "unsynced pits" badge to use a liveQuery instead of checking when the data is synced</li>
+    </ul>
+{/snippet}
+
+<Generic summary={summary} features={features} details={details} />
