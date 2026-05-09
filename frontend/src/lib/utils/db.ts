afd8201d13a1e02ef7b927855327b6b7d3f5e5bf
nfoert <nofoert@gmail.com>
2026-05-09 14:33:49 -0400

    style(frontend): Use 4 spaces for indentation in db.ts

--- frontend/src/lib/utils/db.ts
+++ frontend/src/lib/utils/db.ts
@@ -2,98 +2,98 @@ import Dexie from 'dexie';
 
 // Interfaces for tables
 export interface MatchScoutingData {
-  uuid: string
-  data: any
-  user_uuid: string
-  year: number
-  team_number: number
-  match_number: number 
-  match_type: string
-  event_code: string
-  event_name: string
-  event_type: string
-  event_city: string
-  event_country: string
-  event_start_date: string
-  event_end_date: string
-  synced: boolean
+    uuid: string
+    data: any
+    user_uuid: string
+    year: number
+    team_number: number
+    match_number: number
+    match_type: string
+    event_code: string
+    event_name: string
+    event_type: string
+    event_city: string
+    event_country: string
+    event_start_date: string
+    event_end_date: string
+    synced: boolean
 }
 export interface Season {
-  uuid: string 
-  year: number
-  fields: any
-  game_pieces: any
-  pit_scouting_questions: any
-  fetch_time: Date
+    uuid: string
+    year: number
+    fields: any
+    game_pieces: any
+    pit_scouting_questions: any
+    fetch_time: Date
 }
 export interface Event {
-  uuid: string
-  year: number
-  event_code: string
-  name: string
-  type: string
-  city: string
-  country: string
-  start_date: string
-  end_date: string
-  week: number | null
-  custom: boolean
-  fetch_time: Date
+    uuid: string
+    year: number
+    event_code: string
+    name: string
+    type: string
+    city: string
+    country: string
+    start_date: string
+    end_date: string
+    week: number | null
+    custom: boolean
+    fetch_time: Date
 }
 export interface PitScoutingData {
-  uuid: string
-  answers: any
-  nickname: string
-  team_number: number
-  year: number
-  event_code: string
-  event_name: string
-  event_type: string
-  event_city: string
-  event_country: string
-  event_start_date: string
-  event_end_date: string
-  synced: boolean
+    uuid: string
+    answers: any
+    nickname: string
+    team_number: number
+    year: number
+    event_code: string
+    event_name: string
+    event_type: string
+    event_city: string
+    event_country: string
+    event_start_date: string
+    event_end_date: string
+    synced: boolean
 }
 export interface File {
-  uuid: string
-  data: any
-  url: string
-  synced: boolean
+    uuid: string
+    data: any
+    url: string
+    synced: boolean
 }
 
 // Create DB
 export class OpenScoutingDB extends Dexie {
-  match_scouting! : Dexie.Table<MatchScoutingData>;
-  season_data! : Dexie.Table<Season>;
-  event! : Dexie.Table<Event>;
-  pit_scouting! : Dexie.Table<PitScoutingData>;
-  files! : Dexie.Table<File>;
+    match_scouting!: Dexie.Table<MatchScoutingData>;
+    season_data!: Dexie.Table<Season>;
+    event!: Dexie.Table<Event>;
+    pit_scouting!: Dexie.Table<PitScoutingData>;
+    files!: Dexie.Table<File>;
 
-  constructor() {
-    super('open-scouting');
-    
-    this.version(1).stores({
-      match_scouting: "&uuid, data, user_uuid, year, team_number, match_number, match_type, event_code, event_name, event_type, event_city, event_country, event_start_date, event_end_date, synced",
-      season_data: "$year, fields, game_pieces, pit_scouting_questions, fetch_time",
-      event: "&uuid, year, event_code, name, type, city, country, start_date, end_date, week, custom, fetch_time",
-      pit_scouting: "&uuid, answers, nickname, team_number, year, event_code, event_name, event_type, event_city, event_country, event_start_date, event_end_date, synced",
-      files: "&uuid, data, url, synced"
-    });
-    // Delete entire season_data table when changing to support uuid as primary key
-    // Then sync will re-fetch the items from the server
-    this.version(2).stores({
-      season_data: null
-    });
-    this.version(3).stores({
-      season_data: "&uuid, year, fields, game_pieces, pit_scouting_questions, fetch_time"
-    });
+    constructor() {
+        super('open-scouting');
 
-    this.match_scouting = this.table('match_scouting');
-    this.season_data = this.table('season_data');
-    this.event = this.table('event');
-    this.pit_scouting = this.table('pit_scouting');
-  }
+        this.version(1).stores({
+            match_scouting: "&uuid, data, user_uuid, year, team_number, match_number, match_type, event_code, event_name, event_type, event_city, event_country, event_start_date, event_end_date, synced",
+            season_data: "$year, fields, game_pieces, pit_scouting_questions, fetch_time",
+            event: "&uuid, year, event_code, name, type, city, country, start_date, end_date, week, custom, fetch_time",
+            pit_scouting: "&uuid, answers, nickname, team_number, year, event_code, event_name, event_type, event_city, event_country, event_start_date, event_end_date, synced",
+            files: "&uuid, data, url, synced"
+        });
+        // Delete entire season_data table when changing to support uuid as primary key
+        // Then sync will re-fetch the items from the server
+        this.version(2).stores({
+            season_data: null
+        });
+        this.version(3).stores({
+            season_data: "&uuid, year, fields, game_pieces, pit_scouting_questions, fetch_time"
+        });
+
+        this.match_scouting = this.table('match_scouting');
+        this.season_data = this.table('season_data');
+        this.event = this.table('event');
+        this.pit_scouting = this.table('pit_scouting');
+    }
 }
 
 export const db = new OpenScoutingDB();
