44efc4fd33b29b47f5ca844b07a4c8aeda7cd09a
nfoert <nofoert@gmail.com>
2026-06-08 17:28:18 -0400

    style(frontend): Add comments to user.ts

--- frontend/src/lib/utils/user.ts
+++ frontend/src/lib/utils/user.ts
@@ -4,6 +4,13 @@ import { getUserSettingsUsersMeGetSettingsGet, logoutAuthLogoutPost, updateUserS
 import type { UserResponse } from "$lib/api/model";
 import { toast } from "svelte-sonner";
 
+/**
+ * Get user data from the page
+ * 
+ * Also removes legacy token from local storage
+ * 
+ * @returns The user data
+ */
 function getUser() : UserResponse | null {
     // Remove legacy token from local storage
     if (browser && localStorage.getItem("access_token")) {
@@ -13,10 +20,18 @@ function getUser() : UserResponse | null {
     return page.data.user.user;
 }
 
+/**
+ * Get the user's authentication status
+ * 
+ * @returns True if the user is authenticated
+ */
 function getAuthenticationStatus(): boolean {
     return page.data.user.authenticated;
 }
 
+/**
+ * Sign the user out
+ */
 async function signOut() {
     await logoutAuthLogoutPost().then(async (response) => {
         if (response.status === 200) {
@@ -29,19 +44,47 @@ async function signOut() {
     });
 }
 
+/**
+ * Get the user's settings
+ * 
+ * @returns The user's settings
+ */
 async function getUserSettings() {
     return (await getUserSettingsUsersMeGetSettingsGet()).data;
 }
 
+/**
+ * Set the user's settings
+ * 
+ * TODO: Add types
+ * 
+ * @param settings The user's settings
+ */
 async function setUserSettings(settings) {
     await updateUserSettingsUsersMeUpdateSettingsPost(settings);
 }
 
+/**
+ * Get a user setting
+ * 
+ * TODO: Add types
+ * 
+ * @param key The key of the setting
+ * @returns The value of the setting
+ */
 async function getUserSetting(key) {
     const settings = await getUserSettings();
     return settings[key];
 }
 
+/**
+ * Set a single user setting
+ * 
+ * TODO: Add types
+ * 
+ * @param key The key of the setting
+ * @param value The value of the setting
+ */
 async function setUserSetting(key, value) {
     let settings = await getUserSettings();
     settings[key] = value;
