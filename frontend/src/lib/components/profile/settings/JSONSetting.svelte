b3d8b57539becee416e2e0c83b27d504591eab38
nfoert <nofoert@gmail.com>
2026-06-13 11:24:06 -0400

    feat(frontend): Create components for individual setting types

---
+++ frontend/src/lib/components/profile/settings/JSONSetting.svelte
@@ -0,0 +1,28 @@
+<!-- 
+@component
+A component for rendering a JSON user setting on the profile page
+
+TODO: Support json and list mode
+
+Props:
+    - `setting` (`UserSetting`) - The user setting to render
+    - `getNewSettings` (`() => void`) - A function to get the new settings
+-->
+<script lang="ts">
+	import type { UserSetting } from "$lib/api/model";
+	import Input from "$lib/components/ui/input/input.svelte";
+	import BaseSetting from "./BaseSetting.svelte";
+
+
+    interface Props {
+        setting: UserSetting
+        getNewSettings: () => void
+    }
+    let { setting, getNewSettings }: Props = $props();
+    
+    let newValue = $derived(setting.value);
+</script>
+
+<BaseSetting {setting} newValue={newValue} getNewSettings={getNewSettings}>
+    <Input type="text" name={setting.key} bind:value={newValue} />
+</BaseSetting>
---
+++ frontend/src/lib/components/profile/settings/SettingSectionHeader.svelte
@@ -0,0 +1,15 @@
+<!-- 
+@component
+A header for different setting sections
+
+Props:
+    - `name` (`string`) - The name of the section
+-->
+<script lang="ts">
+    interface Props {
+        name: string
+    }
+    let { name }: Props = $props();
+</script>
+
+<p class="font-bold">{name}</p>
