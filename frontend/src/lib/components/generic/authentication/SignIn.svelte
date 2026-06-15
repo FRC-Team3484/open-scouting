7bc37f7170905316190d30f5686fc12ce6a1e3fd
nfoert <nofoert@gmail.com>
2026-06-15 12:06:10 -0400

    refactor(frontend): Move the sign in conformation card to a seperate component

--- frontend/src/lib/components/generic/authentication/SignIn.svelte
+++ frontend/src/lib/components/generic/authentication/SignIn.svelte
@@ -7,18 +7,16 @@ TODO: Support passkeys
 -->
 <script lang="ts">
 	import { goto } from "$app/navigation";
-	import { onMount } from "svelte";
 	import { slide } from "svelte/transition";
-	import { ArrowLeftIcon, ArrowRightIcon, CheckCircleIcon, CircleNotchIcon, EnvelopeIcon, KeyIcon, WarningIcon } from "phosphor-svelte";
+	import { ArrowLeftIcon, ArrowRightIcon, CircleNotchIcon, EnvelopeIcon, KeyIcon, WarningIcon } from "phosphor-svelte";
 
-    import * as Avatar from "$lib/components/ui/avatar/index";
-    import * as Card from "$lib/components/ui/card/index";
 	import Button from "$lib/components/ui/button/button.svelte";
 	import Input from "$lib/components/ui/input/input.svelte";
     import * as Alert from "$lib/components/ui/alert/index";
 
 	import { loginAuthLoginPost, meAuthMeGet } from "$lib/api/auth/auth";
 	import type { UserResponse } from "$lib/api/model";
+	import SignInConfirmation from "./SignInConfirmation.svelte";
 
 
     let page: "username" | "passkey" | "password" | "success" = $state("username"); 
@@ -30,9 +28,7 @@ TODO: Support passkeys
     let message: string = $state("");
 
     let successUser: UserResponse | null = $state(null);
-    let redirectTimer: number = $state(5);
-
-    let interval: any = $state(null);
+    
 
     /**
      * Sign the user in
@@ -47,16 +43,6 @@ TODO: Support passkeys
                         successUser = response.data.user;
                     }
                 });
-                
-                interval = setInterval(async () => {
-                    redirectTimer -= 1;
-                    
-                    if (redirectTimer == 0) {
-                        clearInterval(interval);
-                        await goto("/");
-                        window.location.reload();
-                    }
-                }, 1000);
                 page = "success";
             } else {
                 message = response.data.detail;
@@ -76,18 +62,11 @@ TODO: Support passkeys
             } else if (page == "password" && password.trim() != "") {
                 signIn();
             } else if (page == "success") {
-                clearInterval(interval);
                 goto("/");
                 window.location.reload();
             }
         }
     }
-
-    onMount(() => {
-        return () => {
-            clearInterval(interval);
-        }
-    })
 </script>
 
 <svelte:window on:keydown={handleKeyDown} />
@@ -138,35 +117,5 @@ TODO: Support passkeys
         </Button>
     </div>
 {:else if page == "success"}
-    <div class="flex flex-col gap-2 text-center items-center my-4" transition:slide>
-        <div class="flex flex-row gap-2 items-center">
-            <CheckCircleIcon weight="bold" />
-            <p class="font-bold">Successfully signed in</p>
-        </div>
-
-        {#if successUser}
-            <div transition:slide>
-                <Card.Root>
-                    <Card.Content>
-                        <div class="flex flex-row gap-2 items-center">
-                            <Avatar.Root class="size-16">
-                                <Avatar.Image src={successUser.profile_picture_url} alt={successUser.username} />
-                                <Avatar.Fallback>{successUser.username.substring(0, 1)}</Avatar.Fallback>
-                            </Avatar.Root>
-                            <div class="flex flex-col gap-1 items-start">
-                                <p>{successUser.display_name}</p>
-                                {#if successUser.username != successUser.display_name}
-                                    <p class="text-muted-foreground">({successUser.username})</p>
-                                {/if}
-                                <p class="text-muted-foreground">{successUser.email}</p>
-                            </div>
-                        </div>
-                    </Card.Content>
-                </Card.Root>
-            </div>
-        {/if}
-
-        <p class="text-muted-foreground animate-pulse">Redirecting home in {redirectTimer}...</p>
-        <Button onclick={async () => {await goto("/"); window.location.reload()}}><ArrowRightIcon weight="bold" /> Home</Button>
-    </div>
+    <SignInConfirmation user={successUser} redirect={5} />
 {/if}
