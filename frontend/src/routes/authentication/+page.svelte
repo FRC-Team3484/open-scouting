306598339cba923df369a4761c7260ab89a5a670
nfoert <nofoert@gmail.com>
2026-06-15 21:46:13 -0400

    feat(frontend): Show a redirecting notice on the authentication page if the user is already signed in

--- frontend/src/routes/authentication/+page.svelte
+++ frontend/src/routes/authentication/+page.svelte
@@ -8,7 +8,7 @@ Allows for the user to sign into their account, or create a new account.
 	import { toast } from "svelte-sonner";
 	import { goto } from "$app/navigation";
 	import { slide } from "svelte/transition";
-	import { ArrowRightIcon } from "phosphor-svelte";
+	import { ArrowRightIcon, CircleNotchIcon } from "phosphor-svelte";
 
 	import Button from "$lib/components/ui/button/button.svelte";
     
@@ -26,29 +26,35 @@ Allows for the user to sign into their account, or create a new account.
      */
     onMount(async () => {
         if (authenticated) {
-            toast.success("You are already signed in, redirecting...");
             await goto("/");
         }
     })
 </script>
 
 <PageContainer>
-    <div class="flex flex-col w-full md:w-1/2 items-center gap-4">
-        <Logo text={false} />
-        <p class="text-2xl font-bold">Authentication</p>
+    {#if !authenticated}
+        <div class="flex flex-col w-full md:w-1/2 items-center gap-4">
+            <Logo text={false} />
+            <p class="text-2xl font-bold">Authentication</p>
 
-        {#if page === "signin"}
-            <div class="flex flex-col gap-4 items-center" transition:slide>
-                <Authentication mode="sign_in" />
-                <Button variant="outline" onclick={() => {page = "signup"}}><ArrowRightIcon weight="bold" /> Sign Up</Button>
-            </div>
+            {#if page === "signin"}
+                <div class="flex flex-col gap-4 items-center" transition:slide>
+                    <Authentication mode="sign_in" />
+                    <Button variant="outline" onclick={() => {page = "signup"}}><ArrowRightIcon weight="bold" /> Sign Up</Button>
+                </div>
 
-        {:else if page === "signup"}
-            <div class="flex flex-col gap-4 items-center" transition:slide>
-                <Authentication mode="create_account" />
-                <Button variant="outline" onclick={() => {page = "signin"}}><ArrowRightIcon weight="bold" /> Sign In</Button>
-            </div>
-        {/if}
-    </div>
-
+            {:else if page === "signup"}
+                <div class="flex flex-col gap-4 items-center" transition:slide>
+                    <Authentication mode="create_account" />
+                    <Button variant="outline" onclick={() => {page = "signin"}}><ArrowRightIcon weight="bold" /> Sign In</Button>
+                </div>
+            {/if}
+        </div>
+    {:else}
+        <div class="flex flex-col gap-2 items-center">
+            <CircleNotchIcon weight="bold" class="animate-spin" size={32} />
+            <p class="text-lg font-bold">You are already signed in</p>
+            <p>Redirecting...</p>
+        </div>
+    {/if}
 </PageContainer>
