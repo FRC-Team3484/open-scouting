837eb25044c677c9cddddf2fe26f3b554666963d
nfoert <nofoert@gmail.com>
2026-07-09 21:48:11 -0400

    feat(frontend): Show the features section on the index page

--- frontend/src/lib/components/index/Welcome.svelte
+++ frontend/src/lib/components/index/Welcome.svelte
@@ -14,7 +14,7 @@ Shows the LiveEvents widget, the logo, a description, and some useful buttons
 	import LiveEvents from "./LiveEvents.svelte";
 </script>
 
-<div class="flex flex-col gap-4 items-center mb-8">
+<div class="flex flex-col gap-4 items-center mb-8 min-h-[calc(100vh-4rem)] justify-center">
     <LiveEvents />
     
     <Logo />
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,10 +4,13 @@ The index page.
 I think it's pretty clear what this does.
 -->
 <script lang="ts">
+	import Features from "$lib/components/index/Features.svelte";
 	import Welcome from "$lib/components/index/Welcome.svelte";
 	import PageContainer from "$lib/components/layout/PageContainer.svelte";
 </script>
 
 <PageContainer>
 	<Welcome />
+
+	<Features />
 </PageContainer>
