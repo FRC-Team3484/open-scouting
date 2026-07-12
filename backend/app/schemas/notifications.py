95c3795f95a5edf1f3552947d4e0d799bb7c09e3
nfoert <nofoert@gmail.com>
2026-07-12 09:55:48 -0400

    feat(backend): Create routes for getting, editing, and adding notifications

---
+++ backend/app/schemas/notifications.py
@@ -0,0 +1,26 @@
+
+from datetime import datetime
+from typing import Any
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class NotificationResponse(BaseModel):
+    uuid: UUID
+    title: str
+    message: str
+    type: str
+    action_type: str | None
+    action_data: dict[Any, Any] | None
+    read: bool
+    created_at: datetime
+
+class NotificationRequest(BaseModel):
+    uuid: UUID
+    title: str
+    message: str
+    type: str
+    action_type: str | None
+    action_data: dict[Any, Any] | None
+    read: bool
