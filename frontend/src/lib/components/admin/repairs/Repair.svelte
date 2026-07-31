a8aa356ac48119ecf5c01a0555a977d1a55fc2b2
nfoert <nofoert@gmail.com>
2026-07-31 14:01:13 -0400

    feat(frontend): Set up repair buttons

--- frontend/src/lib/components/admin/repairs/Repair.svelte
+++ frontend/src/lib/components/admin/repairs/Repair.svelte
@@ -1,10 +1,19 @@
+<!-- 
+@component
+Represents a single repair for the admin page's repair manager
+
+Props:
+    - `repair` (`RepairResponse`) - The repair to show info for
+    - `selectedRepairs` (`RepairResponse[]`) - The selected repairs
+-->
 <script lang="ts">
     import * as Card from "$lib/components/ui/card/index.js";
+	import { TrashIcon } from "phosphor-svelte";
+
+	import Badge from "../../ui/badge/badge.svelte";
+	import { Button } from "$lib/components/ui/button";
 
 	import type { RepairResponse } from "$lib/api/model";
-	import Badge from "../../ui/badge/badge.svelte";
-	import { TrashIcon } from "phosphor-svelte";
-	import { Button } from "$lib/components/ui/button";
 
 
     interface Props {
@@ -24,8 +33,33 @@
                     <Badge>{repair.data_type.charAt(0).toUpperCase() + repair.data_type.replaceAll("_", " ").slice(1)}</Badge>
                     <p class="text-left">{repair.name}</p>
                 </div>
+
                 <p class="text-muted-foreground">Data Created: {new Date(repair.data_created_at).toLocaleString()}</p>
-                <div class="flex flex-row gap-2 flex-wrap">
+
+                <div class="flex flex-row gap-2 flex-wrap">                    
+                    {#if repair.repair_type == "missing_season"}
+                        <Button size="sm">Choose Season</Button>
+
+                    {:else if repair.repair_type == "missing_game_piece"}
+                        <Button size="sm">Choose Game Piece</Button>
+
+                    {:else if repair.repair_type == "missing_event"}
+                        <Button size="sm">Choose Event</Button>
+
+                    {:else if repair.repair_type == "missing_field" && repair.data_type == "match_scouting_answer"}
+                        <Button size="sm">Choose Match Scouting Field</Button>
+
+                    {:else if repair.repair_type == "missing_submission"}
+                        <Button size="sm">Choose Match Scouting Submission</Button>
+
+                    {:else if repair.repair_type == "missing_field" && repair.data_type == "pit_scouting_answer"}
+                        <Button size="sm">Choose Pit Scouting Field</Button>
+
+                    {:else if repair.repair_type == "missing_team"}
+                        <Button size="sm">Choose Team</Button>
+
+                    {/if}
+                    
                     <Button size="sm" variant="destructive"><TrashIcon weight="bold" /> Delete Content</Button>
                 </div>
             </div>
