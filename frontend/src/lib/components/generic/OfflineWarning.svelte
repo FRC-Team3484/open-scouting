ccff685affbb040baefd0abbf73a26fa1bcc670f
nfoert <nofoert@gmail.com>
2026-09-11 20:30:58 -0400

    feat(frontend): Add an offline warning component

---
+++ frontend/src/lib/components/generic/OfflineWarning.svelte
@@ -0,0 +1,28 @@
+<!--
+@component
+A warning when the user is offline
+
+Props:
+    - `text` (`string`) - The text to display
+-->
+<script lang="ts">
+    import * as Card from "$lib/components/ui/card/index.js";
+	import { HouseIcon, WifiSlashIcon } from "phosphor-svelte";
+	import Button from "../ui/button/button.svelte";
+    
+    interface Props {
+        text: string;
+    }
+    let { text }: Props = $props();
+</script>
+<Card.Root class="mt-4">
+    <Card.Content>
+        <div class="flex flex-col gap-2 items-center">
+            <WifiSlashIcon size={32} weight="bold" />
+            <p class="text-xl font-bold">You're Offline</p>
+            <p class="text-md">{text}</p>
+            <p class="text-md">Check your internet connection and try again.</p>
+            <Button variant="default" href="/"><HouseIcon weight="bold" /> Home</Button>
+        </div>
+    </Card.Content>
+</Card.Root>
